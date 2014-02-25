2da84ddf34d95d4fa6f410c39a538e103af43543
vov4ik08 <vov4ik08@gmail.com>
2014-02-25 11:45:16 +0200

    edit basepath to js

--- TinyMceAssets.php
+++ TinyMceAssets.php
@@ -13,7 +13,7 @@ use yii\web\AssetBundle;
 
 class TinyMceAssets extends AssetBundle {
 
-    public $sourcePath = '@vendor/yii2-tinymce/assets';
+    public $sourcePath = '@vendor/apollo/yii2-tinymce/assets';
 
     public $css = [
 
